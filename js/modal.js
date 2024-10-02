ef53c9316983f8d41ff620244fa79b2d104396f9
Chiper Andrei <138249161+chiperandrei42@users.noreply.github.com>
2024-10-02 19:08:52 +0300

    created the navigation bar and a bit of the main page aswell as finishing the mobile modal

---
+++ js/modal.js
@@ -0,0 +1,39 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+    refs.openModalBtn.addEventListener("click", () => {
+      toggleModal();
+        let opacity = 0;
+        const interval = setInterval(() => {
+            if (opacity < 1) {
+                opacity += 0.1;
+                refs.modal.style.opacity = opacity;
+            }
+            else {
+                clearInterval(interval);
+            }
+        }, 10);
+  });
+    refs.closeModalBtn.addEventListener("click", () => {
+        let opacity = 1;
+        const interval = setInterval(() => {
+            if (opacity > 0) {
+                opacity -= 0.1;
+                refs.modal.style.opacity = opacity;
+            }
+            else {
+                clearInterval(interval);
+                toggleModal();
+            }
+        }, 10);
+    });
+    
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    }
+
+})();
