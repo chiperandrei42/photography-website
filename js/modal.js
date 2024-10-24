8da4020e0588728a2d242bf859e85f53ecc2a9cd
Chiper Andrei <138249161+chiperandrei42@users.noreply.github.com>
2024-10-24 21:56:29 +0300

    finished image modal

--- js/modal.js
+++ js/modal.js
@@ -1,39 +1,73 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+        images: document.querySelectorAll("img"),
+        modalDiv: document.querySelector("[image-modal-open]"),
+        body: document.querySelector("body"),
+        imgWrapperModal: document.querySelector(".img-wrapper-modal"),
+        closeImageModalBtn: document.querySelector("[image-modal-close]")
+    };
 
-  refs.openModalBtn.addEventListener("click", () => {
-      toggleModal();
-      let opacity = 0;
+    function fadeIn(element, duration = 10) {
+        element.style.opacity = 0;
+        element.classList.remove("is-hidden");
+        let opacity = 0;
         const interval = setInterval(() => {
-          if (opacity < 1) {
+            if (opacity < 1) {
                 opacity += 0.1;
-                refs.modal.style.opacity = opacity;
+                element.style.opacity = opacity;
+            } else {
+                clearInterval(interval);
             }
-            else {
-              clearInterval(interval);
-            }
-        }, 10);
-  });
-    refs.closeModalBtn.addEventListener("click", () => {
+        }, duration);
+    }
+
+    function fadeOut(element, duration = 10, callback) {
         let opacity = 1;
         const interval = setInterval(() => {
             if (opacity > 0) {
                 opacity -= 0.1;
-                refs.modal.style.opacity = opacity;
-            }
-            else {
+                element.style.opacity = opacity;
+            } else {
                 clearInterval(interval);
-                toggleModal();
+                element.classList.add("is-hidden");
+                if (callback) callback();
             }
-        }, 10);
-    });
-    
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+        }, duration);
     }
 
+    refs.openModalBtn.addEventListener("click", () => {
+        fadeIn(refs.modal);
+    });
+
+    refs.closeModalBtn.addEventListener("click", () => {
+        fadeOut(refs.modal, 10, () => {
+            refs.body.classList.remove("scrolllock");
+        });
+    });
+
+    for (let i = 0; i < refs.images.length; i++) {
+        refs.images[i].addEventListener("click", () => {
+            refs.body.classList.add("scrolllock");
+
+            refs.imgWrapperModal.innerHTML = "";
+
+            let newImg = document.createElement("img");
+            newImg.classList.add("image-modal-clicked");
+            newImg.src = refs.images[i].src;
+            newImg.alt = refs.images[i].alt;
+
+            refs.imgWrapperModal.appendChild(newImg);
+
+            fadeIn(refs.modalDiv);
+        });
+    }
+
+    refs.closeImageModalBtn.addEventListener("click", () => {
+        fadeOut(refs.modalDiv, 10, () => {
+            refs.body.classList.remove("scrolllock");
+        });
+    });
 })();
